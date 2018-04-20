af08b252ff202b1bd4375a33338538ab2312044d
NewBrandSTONE <187336604@qq.com>
2018-04-20 16:24:20 +0800

    完成主页的Adapter和RecyclerView的编写

--- t-core/src/main/java/com/dahua/oz/t/core/ui/refresh/RefreshHandler.java
+++ t-core/src/main/java/com/dahua/oz/t/core/ui/refresh/RefreshHandler.java
@@ -1,11 +1,15 @@
 package com.dahua.oz.t.core.ui.refresh;
 
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
 
+import com.alibaba.fastjson.JSONObject;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.dahua.oz.t.core.app.Traffic;
 import com.dahua.oz.t.core.net.RestClient;
 import com.dahua.oz.t.core.net.callback.ISuccess;
+import com.dahua.oz.t.core.ui.recycler.AbstractDataConverter;
+import com.dahua.oz.t.core.ui.recycler.MultipleRecyclerAdapter;
 
 /**
  * SwipRefreshLayout的刷新方法
@@ -14,14 +18,30 @@ import com.dahua.oz.t.core.net.callback.ISuccess;
  * @version 2018/4/18
  */
 
-public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
+public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener,
+        BaseQuickAdapter.RequestLoadMoreListener {
 
     private final SwipeRefreshLayout REFRESH_LAYOUT;
+    private final PagingBean BEAN;
+    private final RecyclerView RECYCLERVIEW;
+    private final AbstractDataConverter CONVERTER;
+    private MultipleRecyclerAdapter mAdapter = null;
 
-    public RefreshHandler(SwipeRefreshLayout refreshLayout) {
+
+    private RefreshHandler(SwipeRefreshLayout refreshLayout, RecyclerView recyclerView,
+                           AbstractDataConverter converter, PagingBean bean) {
         this.REFRESH_LAYOUT = refreshLayout;
+        this.RECYCLERVIEW = recyclerView;
+        this.CONVERTER = converter;
+        this.BEAN = bean;
         REFRESH_LAYOUT.setOnRefreshListener(this);
     }
+
+    public static RefreshHandler create(SwipeRefreshLayout refreshLayout, RecyclerView recyclerView,
+                                        AbstractDataConverter converter) {
+        return new RefreshHandler(refreshLayout, recyclerView, converter, new PagingBean());
+    }
+
 
     private void refresh() {
         REFRESH_LAYOUT.setRefreshing(true);
@@ -42,15 +62,29 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
     }
 
     public void firstPage(String url) {
+        // 增加延迟效果
+        BEAN.setDelayed(1000);
         RestClient.builder()
                 .url(url)
                 .success(new ISuccess() {
                     @Override
                     public void onSuccess(String response) {
-                        Toast.makeText(Traffic.getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        JSONObject jsonObject = JSONObject.parseObject(response);
+                        BEAN.setTotal(jsonObject.getInteger("total"))
+                                .setPageSize(jsonObject.getInteger("page_size"));
+                        // 设置Adapter
+                        mAdapter = MultipleRecyclerAdapter.create(CONVERTER.setJsonData(response));
+                        mAdapter.setOnLoadMoreListener(RefreshHandler.this, RECYCLERVIEW);
+                        RECYCLERVIEW.setAdapter(mAdapter);
+                        BEAN.addIndex();
                     }
                 })
                 .build()
                 .get();
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+
     }
 }
--- t-traffic/src/main/java/com/dahua/oz/t/traffic/main/index/IndexDelegate.java
+++ t-traffic/src/main/java/com/dahua/oz/t/traffic/main/index/IndexDelegate.java
@@ -4,22 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
 import com.dahua.oz.t.core.bottom.BottomItemDelegate;
-import com.dahua.oz.t.core.net.RestClient;
-import com.dahua.oz.t.core.net.callback.ISuccess;
-import com.dahua.oz.t.core.ui.recycler.MultipleFields;
-import com.dahua.oz.t.core.ui.recycler.MultipleItemEntity;
 import com.dahua.oz.t.core.ui.refresh.RefreshHandler;
 import com.dahua.oz.t.traffic.R;
 import com.dahua.oz.t.traffic.R2;
 import com.joanzapata.iconify.widget.IconTextView;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 
@@ -55,7 +49,8 @@ public class IndexDelegate extends BottomItemDelegate {
     public void onLazyInitView(@Nullable Bundle savedInstanceState) {
         super.onLazyInitView(savedInstanceState);
         initRefreshLayout();
-//        mRefreshHandler.firstPage("http://192.168.31.230:8080/index");
+        initRecyclerView();
+        mRefreshHandler.firstPage("http://192.168.31.230:8080/index");
     }
 
     @Override
@@ -65,20 +60,11 @@ public class IndexDelegate extends BottomItemDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-        mRefreshHandler = new RefreshHandler(mRefreshLayout);
-        RestClient.builder()
-                .url("http://192.168.31.230:8080/index")
-                .success(new ISuccess() {
-                    @Override
-                    public void onSuccess(String response) {
-                        IndexDataConverter converter = new IndexDataConverter();
-                        converter.setJsonData(response);
-                        ArrayList<MultipleItemEntity> datas = converter.convert();
-                        String text = datas.get(1).getField(MultipleFields.TEXT);
-                        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .build()
-                .get();
+        mRefreshHandler = RefreshHandler.create(mRefreshLayout, mRecyclerView, new IndexDataConverter());
+    }
+
+    private void initRecyclerView() {
+        final GridLayoutManager manager = new GridLayoutManager(getContext(), 4);
+        mRecyclerView.setLayoutManager(manager);
     }
 }
